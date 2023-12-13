4fc30b0c6856a684e49943edcd0d50666ded863b
ana carolina <carolina-macedo93@hotmail.com>
2023-12-13 19:59:00 -0300

    finalização-pokedex-api

--- js/poke-api.js
+++ js/poke-api.js
@@ -1,14 +1,13 @@
-
 const pokeApi = {}
 
-function convertPokeApiDetailTOPokemon(pokeDetail) {
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
+
     pokemon.types = types
     pokemon.type = type
 
@@ -19,20 +18,17 @@ function convertPokeApiDetailTOPokemon(pokeDetail) {
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailTOPokemon)
-
-    }
-
-
-pokeApi.getPokemons = (offset = 0, limit = 5) => {    
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
-
-    return fetch(url)
-     .then((response) => response.json())
-     .then((jsonBody) => jsonBody.results)
-     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-     .then((detailRequest) => Promise.all(detailRequest))
-     .then((pokemonsDetails) => pokemonsDetails)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
